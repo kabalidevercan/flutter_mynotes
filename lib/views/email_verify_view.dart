657349a86afb85953b12cd69f650c8d60c2c8f5d
kabalidevercan <kabalidevercan@gmail.com>
2022-06-25 12:47:22 +0300

    step-7

--- lib/views/email_verify_view.dart
+++ lib/views/email_verify_view.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_yeniden_ogreniyorum/constants/routes.dart';
 
 class EmailVerifyView extends StatefulWidget {
-  EmailVerifyView({Key? key}) : super(key: key);
+  const EmailVerifyView({Key? key}) : super(key: key);
 
   @override
   State<EmailVerifyView> createState() => _EmailVerifyViewState();
@@ -19,14 +20,29 @@ class _EmailVerifyViewState extends State<EmailVerifyView> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text("Please Verify Email"),
+            const Text(
+              "We've sent you an email verification.Please open it to verify your account.",
+            ),
+            const Text(
+              "If you haven't received a verification email yet,press the button below",
+            ),
             ElevatedButton(
               onPressed: () async {
                 final user = FirebaseAuth.instance.currentUser;
                 await user!.sendEmailVerification();
               },
               child: const Text("Send email verification"),
-            )
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                  registerRoute,
+                  (route) => false,
+                );
+              },
+              child: const Text("Restart"),
+            ),
           ],
         ),
       ),
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -3,7 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_yeniden_ogreniyorum/constants/routes.dart';
 import 'package:flutter_yeniden_ogreniyorum/firebase_options.dart';
-import 'dart:developer' as devtools show log;
+
+import 'package:flutter_yeniden_ogreniyorum/widgets/show_error_dialog.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -61,19 +62,25 @@ class _RegisterViewState extends State<RegisterView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                final user = await FirebaseAuth.instance
+                final userCredential = await FirebaseAuth.instance
                     .createUserWithEmailAndPassword(
                         email: email, password: password);
-                devtools.log(user.toString());
+                final user = FirebaseAuth.instance.currentUser;
+                await user?.sendEmailVerification();
+                Navigator.of(context).pushNamed(verifyEmailRoute);
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'weak-password') {
-                  devtools.log("Sağlam Şifre girsene ");
+                  await showErrorDialog(context, "Weak Password");
                 } else if (e.code == 'email-already-in-use') {
-                  devtools
-                      .log("Aynı maille kayıt olmuşssun kimi kandırıyon koçum");
+                  await showErrorDialog(context, "Email already in use");
                 } else if (e.code == 'invalid-email') {
-                  devtools.log("Geçersiz Email");
+                  await showErrorDialog(
+                      context, "This is an invalid email adress");
+                } else {
+                  await showErrorDialog(context, "Error: ${e.code}");
                 }
+              } catch (e) {
+                await showErrorDialog(context, "Error: ${e.toString()}");
               }
             },
             child: const Text("Register"),
